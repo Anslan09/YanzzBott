9ca54f9479f9fd1a530f3ff1aff90ea75ecfa2da
Euoon <72862552+eueon@users.noreply.github.com>
2022-12-17 12:13:09 +0800

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -402,24 +402,11 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
         )
 
 
-    elif query.data == "music_":
+        elif query.data == "music_":
         query.message.edit_text(
-            text= """
-✅Play Commands:
-Available Commands = play , vplay , cplay
-ForcePlay Commands = playforce , vplayforce , cplayforce
-c stands for channel play.
-v stands for video play.
-force stands for force play.
-/play or /vplay or /cplay  - Bot will start playing your given query on voice chat or Stream live links on voice chats.
-/playforce or /vplayforce or /cplayforce -  Force Play stops the current playing track on voice chat and starts playing the searched track instantly without disturbing/clearing queue.
-/channelplay [Chat username or id] or [Disable] - Connect channel to a group and stream music on channel's voice chat from your group.
-✅Bot's Server Playlists:
-/playlist  - Check Your Saved Playlist On Servers.
-/deleteplaylist - Delete any saved music in your playlist
-/play  - Start playing Your Saved Playlist from Servers.
-""",
-parse_mode=ParseMode.MARKDOWN,
+            text="*๏ Menu Music.*"
+            f"\n\nKlik button di bawah.",
+            parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(
                 [
                     [
